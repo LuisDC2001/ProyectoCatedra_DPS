894e9c0ec9a290284d3d3caadffe71be3b395c1f
Ortega02 <io.ortega.tony@gmail.com>
2023-10-04 21:30:46 -0600

    Animación en demás páginas

--- RentAndGo/screens/ReservationsScreen.js
+++ RentAndGo/screens/ReservationsScreen.js
@@ -1,33 +1,63 @@
-import React, { useState } from "react";
-import {
-  View,
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  StyleSheet,
-} from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome"; 
+import React, { useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
 
-const ReservationsScreen = () => {
+import LottieView from 'lottie-react-native';
+import tw from 'twrnc';
+
+
+
+const FilterScreen = () => {
+
+  const lottieAnim = useRef(null);
+
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  useEffect(() => {
+    // Iniciar la animación de la animación Lottie
+    lottieAnim.current.play();
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Esta es la página de reservaciones</Text>
+      <TouchableOpacity style={styles.goBackButton} onPress={handleGoBack}>
+        <Icon name="close" size={24} />
+      </TouchableOpacity>
+      <LottieView
+        ref={lottieAnim}
+        source={require('../assets/under.json')}
+        autoPlay
+        loop
+        style={tw`w-[80] h-[80]`}
+      />
+      <Text style={styles.text}>Página de Reservas en construcción</Text>
     </View>
+    
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  goBackButton: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    borderRadius: 25,
+    padding: 10,
   },
 });
 
-export default ReservationsScreen;
+export default FilterScreen;
