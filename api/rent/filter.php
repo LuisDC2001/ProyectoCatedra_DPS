27fc292eb9eaba46e4c6efa6f996e08d3d24e80e
febonilla16 <fernandobonilla875@gmail.com>
2023-11-01 22:43:07 -0600

    Modificación API de filtro para alquileres o reservas

--- api/rent/filter.php
+++ api/rent/filter.php
@@ -15,30 +15,6 @@
         $dbModel = new Model();
         $filterRents = array();
         $search = emptyStringToNull($search);
-        $filterTypeOfCar = "
-            SELECT tv.nombre
-            FROM tipovehiculo AS tv
-            WHERE tv.fechaFila <= NOW() AND tv.nombre LIKE '".$search['tipoVehiculo']."'";
-        $filterBrand = "
-            SELECT m.nombre
-            FROM marca AS m
-            WHERE m.fechaFila <= NOW() AND m.nombre LIKE '".$search['marca']."'";
-        $filterTransmition = "
-            SELECT t.nombre
-            FROM transmision AS t
-            WHERE t.fechaFila <= NOW() AND t.nombre LIKE '".$search['transmision']."'";
-        $filterYear = "
-            SELECT v.year
-            FROM vehiculo AS v
-            WHERE v.fechaFila <= NOW() AND (v.year BETWEEN '".$search['añoMinimo']."' AND '".$search['añoMaximo']."')";
-        $filterPassengers = "
-            SELECT v.pasajeros
-            FROM vehiculo AS v
-            WHERE v.fechaFila <= NOW() AND (v.pasajeros BETWEEN '".$search['pasajerosMinimo']."' AND '".$search['pasajerosMaximo']."')";
-        $filterPrices = "
-            SELECT r.precioDia
-            FROM reserva AS r
-            WHERE r.fechaFila <= NOW() AND (r.precioDia BETWEEN '".$search['precioMinimo']."' AND '".$search['precioMaximo']."')";  
         $query = "
             SELECT r.cantidadDias, 
                    r.precioDia, 
@@ -87,23 +63,71 @@
             WHERE r.disponible = 1 
                   AND r.fechaFila <= NOW()";
         if (isset($search['tipoVehiculo'])) {
-            $query .= "AND tv.nombre IN ($filterTypeOfCar)";
+            $filterTypeOfCar = "
+                SELECT DISTINCT (tv.nombre)
+                FROM tipovehiculo AS tv
+                WHERE tv.fechaFila <= NOW() AND tv.nombre LIKE '".$search['tipoVehiculo']."'";
+            $query .= " AND tv.nombre IN ($filterTypeOfCar)";
         }
         if (isset($search['marca'])) {
-            $query .= "AND ma.nombre IN ($filterBrand)";
+            $filterBrand = "
+                SELECT DISTINCT (m.nombre)
+                FROM marca AS m
+                WHERE m.fechaFila <= NOW() AND m.nombre LIKE '".$search['marca']."'";
+            $query .= " AND ma.nombre IN ($filterBrand)";
         }
         if (isset($search['transmision'])) {
-            $query .= "AND v.transmision IN ($filterTransmition)";
+            $filterTransmition = "
+                SELECT DISTINCT (t.nombre)
+                FROM transmision AS t
+                WHERE t.fechaFila <= NOW() AND t.nombre LIKE '".$search['transmision']."'";
+            $query .= " AND t.nombre IN ($filterTransmition)";
         }
-        if (isset($search['año'])) {
-            $query .= "AND v.year IN ($filterYear)";
-        }
-        if (isset($search['pasajeros'])) {
-            $query .= "AND v.pasajeros IN ($filterPassengers)";
-        }
-        if (isset($search['precioMinimo'])) {
-            $query .= "AND r.precioDia IN ($filterPrices)";
-        }
+        $yearMin = "
+            SELECT DISTINCT (MIN(v.year))
+            FROM vehiculo AS v
+            WHERE v.fechaFila <= NOW()";
+        $yearMax = "
+            SELECT DISTINCT (MAX(v.year))
+            FROM vehiculo AS v
+            WHERE v.fechaFila <= NOW()";
+        $yearMin = (isset($search['añoMinimo'])) ? intval($search['añoMinimo']) : $yearMin ;
+        $yearMax = (isset($search['añoMaximo'])) ? intval($search['añoMaximo']) : $yearMax ;
+        $filterYear = "
+            SELECT DISTINCT (v.year)
+            FROM vehiculo AS v
+            WHERE v.fechaFila <= NOW() AND (v.year BETWEEN (".$yearMin.") AND (".$yearMax."))";
+        $query .= " AND v.year IN ($filterYear)";
+        $passengersMin = "
+            SELECT DISTINCT (MIN(v.pasajeros))
+            FROM vehiculo AS v
+            WHERE v.fechaFila <= NOW()";
+        $passengersMax = "
+            SELECT DISTINCT (MAX(v.pasajeros))
+            FROM vehiculo AS v
+            WHERE v.fechaFila <= NOW()";
+        $passengersMin = (isset($search['pasajerosMinimo'])) ? intval($search['pasajerosMinimo']) : $passengersMin ;
+        $passengersMax = (isset($search['pasajerosMaximo'])) ? intval($search['pasajerosMaximo']) : $passengersMax ;
+        $filterPassengers = "
+            SELECT DISTINCT (v.pasajeros)
+            FROM vehiculo AS v
+            WHERE v.fechaFila <= NOW() AND (v.pasajeros BETWEEN (".$passengersMin.") AND (".$passengersMax."))";
+        $query .= " AND v.pasajeros IN ($filterPassengers)";
+        $priceMin = "
+            SELECT DISTINCT (MIN(r.precioDia))
+            FROM reserva AS r
+            WHERE r.fechaFila <= NOW()";
+        $priceMax = "
+            SELECT DISTINCT (MAX(r.precioDia))
+            FROM reserva AS r
+            WHERE r.fechaFila <= NOW()";
+        $priceMin = (isset($search['precioMinimo'])) ? intval($search['precioMinimo']) : $priceMin ;
+        $priceMax = (isset($search['precioMaximo'])) ? intval($search['precioMaximo']) : $priceMax ;
+        $filterPrices = "
+            SELECT DISTINCT (r.precioDia)
+            FROM reserva AS r
+            WHERE r.fechaFila <= NOW() AND (r.precioDia BETWEEN (".$priceMin.") AND (".$priceMax."))";  
+        $query .= " AND r.precioDia IN ($filterPrices)";
         $query .= "ORDER BY r.fechaFila DESC ;";
         $filterRents = $dbModel->getQuery($query);
         foreach ($filterRents as $key => $value) {
